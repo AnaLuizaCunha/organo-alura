a024639fe898a4de1695f225d3ecac71cf317e8c
AnaLuizaCunha <ana.luiza.cunha@id.uff.br>
2024-11-14 11:01:30 -0300

    created Button component

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,14 +1,37 @@
 import "./Form.css";
 import TextField from "../TextField";
+import Dropdown from "../Dropdown";
+import Button from "../Button";
 
 const Form = () => {
+  const team = [
+    "Programação",
+    "Front-end",
+    "Data Science",
+    "Devops",
+    "Ux e Design",
+    "Mobile",
+    "Inovação e Gestão",
+  ];
+
+  const onSave = (event) => {
+    event.preventDefault();
+    console.log("Form foi submetido");
+  };
+
   return (
     <section className="form">
-      <form>
+      <form onSubmit={onSave}>
         <h2>Preencha os dados para criar o card do colaborador</h2>
-        <TextField label="Nome" placeholder="Digite seu nome" />
-        <TextField label="Cargo" placeholder="Digite seu cargo" />
+        <TextField required={true} label="Nome" placeholder="Digite seu nome" />
+        <TextField
+          required={true}
+          label="Cargo"
+          placeholder="Digite seu cargo"
+        />
         <TextField label="Imagem" placeholder="Digite o endereço da imagem" />
+        <Dropdown required={true} label="Time" itens={team} />
+        <Button>Criar Card</Button>
       </form>
     </section>
   );
